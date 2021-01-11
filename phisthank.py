36ce81d677836191dbc473ec9023013eb5bcaf3c
st4unch <murat-aydogan@outlook.com>
2021-01-11 13:29:19 +0300

    Update phisthank.py

--- phisthank.py
+++ phisthank.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import requests
 import  gzip
 import shutil
@@ -36,7 +37,7 @@ def unzip():
     if response.status_code<400 :
         writefile = open(fil, 'wb').write(response.content)
         wf=str(writefile)
-        logger.info("phistank status is ",wf)
+        logger.info("phistank status is "+wf)
         return fil, writefile
     else:
         writefile="False"
@@ -45,24 +46,15 @@ def unzip():
 
 
 def main():
-    uz=str(unzip())
-    f=str(uz[0])
-    f2=uz[1]
-    if type(f2) is str:
-        logger.info('msg:file response is lower than expected')
-        f2=1
-    if uz != "False" and f2>9999:
-        file = f
-        file2 = file.strip('.gz')
-        with gzip.open(file, 'rb') as filein:
+    uza=unzip()
+    uz=uza[0]
+    f2=int(uza[1])
+    print(uz,f2)
+    if uz != False and f2>9999:
+        file2 = uz.strip('.gz')
+        with gzip.open(uz, 'rb') as filein:
             with open(f'{folderpath}'+file2, 'wb') as fileout:
-                print(shutil.copyfileobj(filein, fileout))
-        checkfile = os.path.getsize(file2)
-        chekthz = str(os.path.exists(file))
-        if chekthz == "False":
-            logger.info("msg: " + file + " deleted")
-        if checkfile > 1:
-            logger.info("msg: file created successfully")
+                shutil.copyfileobj(filein, fileout)
     else:
         if uz == "False":
             logger.info("the unzip response is  false please check the domain ")
